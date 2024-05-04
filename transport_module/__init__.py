8977d1cb8287926ce73ec95fd2c1beab1d979c10
TectoArc <89665821+TectoArc@users.noreply.github.com>
2024-05-04 19:22:07 +0300

    Add files via upload

--- transport_module/__init__.py
+++ transport_module/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["field", "flow_solver", "particletracking"]
 
-from pyddc.transport_module import field
-from pyddc.transport_module import flow_solver
-from pyddc.transport_module import particletracking
+# from pyddc.transport_module import field
+# from pyddc.transport_module import flow_solver
+# from pyddc.transport_module import particletracking
